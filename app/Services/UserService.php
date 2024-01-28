519f3c2419e7dfa0d14061cc8abe0cd55c923abc
Stryker <testexample.com>
2024-01-28 18:03:27 +0200

    fix create()

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -2,16 +2,15 @@
 
 namespace App\Services;
 
-use App\Http\Requests\User\StoreUserRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 class UserService
 {
-    public function create(StoreUserRequest $request): UserResource
+    public function create(array $data): UserResource
     {
-        return User::create($request->validated());
+        return User::create($data);
     }
 
     public function getUsersWithTasks(): LengthAwarePaginator
